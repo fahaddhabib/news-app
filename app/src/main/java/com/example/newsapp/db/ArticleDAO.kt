9b419e2beace90bc6dd6e61bdb12afbb3949ff50
fahaddhabib <iamfahad1296@gmail.com>
2024-01-02 01:28:46 +0500

    DataBase Article Added

---
+++ app/src/main/java/com/example/newsapp/db/ArticleDAO.kt
@@ -0,0 +1,22 @@
+package com.example.newsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.newsapp.models.Article
+import retrofit2.http.DELETE
+
+@Dao
+interface ArticleDAO {
+
+    @Insert(
+        onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+    @Query("SELECT*FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @DELETE
+    suspend fun deleteArticle(article: Article)
+}
--- app/src/main/java/com/example/newsapp/models/Article.kt
+++ app/src/main/java/com/example/newsapp/models/Article.kt
@@ -1,6 +1,15 @@
 package com.example.newsapp.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
@@ -9,4 +18,4 @@ data class Article(
     val title: String,
     val url: String,
     val urlToImage: String
-)
+): Serializable
